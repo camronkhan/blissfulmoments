d1fb6cc616b88c5bd82a4d6f74d11e61e432ce5d
Camron Khan <camron.khan@gmail.com>
2016-08-05 09:12:38 -0500

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,13 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
 
-  get 'static_pages/about'
+  	root 'static_pages#home'
+  
+	get 	'home',       	to: 'static_pages#home',        as: 'home'
+	get     'about',      	to: 'static_pages#about',       as: 'about'
+	get     'investment',	to: 'static_pages#investment',	as: 'investment'
+	get     'studio', 	  	to: 'static_pages#studio',  	as: 'studio'
+	get     'contact',    	to: 'messages#new',             as: 'contact'
+	post    'contact',    	to: 'messages#create'
+	get     'clients',      to: 'static_pages#clients',		as: 'clients'
 
-  get 'static_pages/investment'
-
-  get 'static_pages/studio'
-
-  get 'static_pages/contact'
-
-  get 'static_pages/clients'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
