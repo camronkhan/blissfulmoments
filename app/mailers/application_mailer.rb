5ee781b3e44f081c1cbf9bdfbaef823fbae6faf6
Camron Khan <camron.khan@gmail.com>
2016-08-12 11:57:50 -0500

    added paypal instructions; added contact form

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,9 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "contact-form@blissfulmomentsbystephanie.com"
-  default to: "Stephanie <stephanie@blissfulmomentsbystephanie.com>"
+  #default from: "contact-form@blissfulmomentsbystephanie.com"
+  #default to: "Stephanie <stephanie@blissfulmomentsbystephanie.com>"
+  #layout 'mailer'
+
+  default from: "contact-form@camronkhan.com"
+  default to: "Camron Khan <camron.khan@gmail.com>"
   layout 'mailer'
 end
