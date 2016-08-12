4001d8e2d6e86f177ac08829106d779424f7cc84
Camron Khan <camron.khan@gmail.com>
2016-08-12 12:27:48 -0500

    updated mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -3,7 +3,7 @@ class ApplicationMailer < ActionMailer::Base
   #default to: "Stephanie <stephanie@blissfulmomentsbystephanie.com>"
   #layout 'mailer'
 
-  default from: "contact-form@camronkhan.com"
-  default to: "Camron Khan <camron.khan@gmail.com>"
+  default from: "contact-form@blissfulmomentsbystephanie.com"
+  default to: "Stephanie <stephanie@blissfulmomentsbystephanie.com>"
   layout 'mailer'
 end
